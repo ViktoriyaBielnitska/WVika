9f6d36b1d499d1a7e77edc8ab7ec55832f755853
EC2 Default User <ec2-user@ip-172-31-30-61.eu-north-1.compute.internal>
2025-06-23 09:52:17 +0000

    Add PostgreSQL connection and psycopg2

--- app.py
+++ app.py
@@ -5,3 +5,23 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello from AWS + Nginx + Flask!"
+
+
+import os
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+@app.route("/")
+def index():
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute("SELECT NOW();")
+    now = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f"PostgreSQL time: {now}"
+
